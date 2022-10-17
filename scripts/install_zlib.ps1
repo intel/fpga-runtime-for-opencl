ef04f6e54cb956e68ab41ba2f8bddf2b9e491a36
Peter Colberg <peter.colberg@intel.com>
2022-10-17 18:06:32 -0400

    Update zlib to 1.2.13
    Signed-off-by: Peter Colberg <peter.colberg@intel.com>
    (cherry picked from commit 4360aa8809c129c8404fe166db8e8e4aad07ac40)

--- scripts/install_zlib.ps1
+++ scripts/install_zlib.ps1
@@ -6,11 +6,11 @@
 
 Param(
   [Parameter(Mandatory)]$installdir,
-  $version="1.2.12",
+  $version="1.2.13",
   $builddir="zlib-$version",
   $archive="zlib-$version.tar.gz",
   $archive_url="https://zlib.net/fossils/$archive",
-  $sha256="91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
+  $sha256="b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
 )
 
 $ErrorActionPreference = "Stop"
